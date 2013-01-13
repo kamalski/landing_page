356256e1e62d0abbf43fc0d4b4122726a9e789f3
Mohamed <kamal@gigturn.com>
2013-01-12 20:57:38 -0500

    enable admin invitations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ RailsPrelaunchSignup::Application.routes.draw do
   end
   devise_scope :user do
     root :to => "devise/registrations#new"
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
   devise_for :users, :controllers => { :registrations => "registrations" }
-  resources :users
+  match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
+  resources :users do
+    get 'invite', :on => :member
+  end
 end
